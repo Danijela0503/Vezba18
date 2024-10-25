830bfe9d48d4be6c9c65ffd4daadcce647cafcca
Danijela Oljaca <danijela.oljaca@nfinnova.com>
2024-10-25 17:38:08 +0200

    Domaci Vezba 18

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String [] Znakovi = { "Heart","Cherry","Coin","Melon","Jocker" };
+
+        System.out.println("Combination is: " );
+        System.out.println(Znakovi[4] + " "+ Znakovi[3]+ " "+Znakovi[3]+ " "+Znakovi[0]+" "+Znakovi[2]);
+
+    }
+}
